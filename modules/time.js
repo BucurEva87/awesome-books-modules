e0eea81ec5b88426746aaf221bd3225aba4c978d
Eva Lavinia Bucur <64814566+BucurEva87@users.noreply.github.com>
2022-08-11 15:21:31 +0300

    Update time.js

--- modules/time.js
+++ modules/time.js
@@ -1,7 +1,7 @@
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
 import utils from './utils.js';
 
-export default resolveDate = () => {
+export default () => {
   const now = DateTime.now();
 
   utils.qs('header > p').textContent = `${now.toLocaleString(
