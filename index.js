b5bad4a52badd74bed1ecbecb1ed382f6b1e8654
BucurEva87 <bucur.eva87@gmail.com>
2022-08-11 15:01:40 +0300

    Adapt JavaScript to using modules and ES6 syntax

---
+++ index.js
@@ -0,0 +1,11 @@
+import { resolveDate } from './modules/time.js';
+import {
+  addRemoveEvents,
+  addSubmitEvent,
+  addClickEvents,
+} from './modules/events.js';
+
+resolveDate();
+addRemoveEvents();
+addSubmitEvent();
+addClickEvents();
